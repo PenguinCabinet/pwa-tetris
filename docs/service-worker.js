f8ff5f849a224627d21437910261a246d032a056
pengincoalition <kage2.kage22@gmail.com>
2021-05-21 10:55:01 +0900

    default Japanese

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pwa-tetris/precache-manifest.4b5f35cef008edfae113d14a46b99fd1.js"
+  "/pwa-tetris/precache-manifest.b79f103fd29bed94d17e33caa5d2b8a6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pwa-tetris"});
